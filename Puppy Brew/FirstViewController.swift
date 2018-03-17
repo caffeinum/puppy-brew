c6c7f733dc1dc70d49e2867133777f7a69914b37
Aleks Bykhun <theisease@gmail.com>
2018-03-17 15:50:26 +0300

    Initial Commit

---
+++ Puppy Brew/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Puppy Brew
+//
+//  Created by Aleksey Bykhun on 17.03.2018.
+//  Copyright © 2018 caffeinum. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
