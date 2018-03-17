592583c9dc5948e03c557ab472607a5d2fbbec04
Aleks Bykhun <theisease@gmail.com>
2018-03-17 19:27:39 +0300

    Display list of Brews on first tab

--- Puppy Brew/FirstViewController.swift
+++ Puppy Brew/FirstViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let cellIdentifier = "BrewCellIdentifier"
+    var controller = BrewController()
+    var brews: [Brewery] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.dataSource = self
+        brews = controller.reload()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension FirstViewController: UITableViewDataSource {
+    func add(brew: Brewery) {
+        controller.store(brew: brew)
+        brews = controller.reload()
+        tableView.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return brews.count
+        } else {
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) else {
+            fatalError("Cannot dequeue cell!")
+        }
+
+        cell.textLabel?.text = String(describing: brews[indexPath.row])
+        cell.textLabel?.numberOfLines = 3
+
+        return cell
+    }
+}
